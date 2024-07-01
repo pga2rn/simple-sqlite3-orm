adf1dd2710d1946f1abb85c9816221e3dfe6cb98
pga2rn <aaronpigybd@gmail.com>
2024-07-02 00:34:11 +0900

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,6 @@ import string
 import time
 from typing import get_args
 
-import pytest
-
 from tests.sample_db.table import (
     Choice123,
     ChoiceABC,
@@ -20,16 +18,23 @@ from tests.sample_db.table import (
 # for reproducible test
 random.seed(0)
 
+# test config
 
-def _generate_random_str(_len: int = 128) -> str:
+TEST_ENTRY_NUM = 6_000
+TEST_REMOVE_ENTRIES_NUM = 128
+TEST_LOOKUP_ENTRIES_NUM = 128
+PRIM_KEY_LEN = 128
+TABLE_NAME = "test_table"
+
+
+def _generate_random_str(_len: int = PRIM_KEY_LEN) -> str:
     """Generate a random string with <_len> characters.
     NOTE: this is NOT safe for generating a password!
     """
     return "".join(random.choice(string.printable) for _ in range(_len))
 
 
-@pytest.fixture(scope="session")
-def generate_testdata(num_of_entry: int) -> dict[str, SampleTable]:
+def generate_test_data(num_of_entry: int) -> dict[str, SampleTable]:
     res: dict[str, SampleTable] = {}
     for idx in range(num_of_entry):
         while True:  # generate a unique prim_key
@@ -37,12 +42,11 @@ def generate_testdata(num_of_entry: int) -> dict[str, SampleTable]:
             if prim_key not in res:
                 break
 
-        now = datetime.datetime.fromtimestamp(time.time())
-
+        now_timestamp = time.time()
         res[prim_key] = SampleTable(
-            unix_timestamp=now,
-            unix_timestamp_int=now,
-            datetime_iso8601=now,
+            unix_timestamp=datetime.datetime.fromtimestamp(now_timestamp),
+            unix_timestamp_int=datetime.datetime.fromtimestamp(int(now_timestamp)),
+            datetime_iso8601=datetime.datetime.fromtimestamp(now_timestamp),
             choice_abc=random.choice(list(ChoiceABC)),
             optional_choice_123=random.choice(list(Choice123) + [None]),
             optional_num_literal=random.choice(list(get_args(SomeIntLiteral)) + [None]),
