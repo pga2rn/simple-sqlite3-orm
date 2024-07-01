30f75fcf1809d47286b154877e09b585c6822f51
pga2rn <aaronpigybd@gmail.com>
2024-07-01 13:25:23 +0900

    types: fix DatetimeUnixTimestamp related

--- src/simple_sqlite3_orm/_types.py
+++ src/simple_sqlite3_orm/_types.py
@@ -16,29 +16,30 @@ from typing_extensions import Annotated
 def _datetime_validator(_in: int | float | str | datetime.datetime):
     if isinstance(_in, datetime.datetime):
         return _in
-    if isinstance(_in, (int, float)):
+    elif isinstance(_in, (int, float)):
         return datetime.datetime.fromtimestamp(_in)
-    if isinstance(_in, str):
+    elif isinstance(_in, str):
         return datetime.datetime.fromisoformat(_in)
-    raise ValueError(f"{_in=} is not a valid datetime")
+    else:
+        raise ValueError(f"{_in=} is not a valid datetime")
 
 
 # NOTE(20240402): pydantic's datetime parsing will add tzinfo into the
 #   validated result. This behavior is different from datetime.fromtimestamp.
 #   we don't want this behavior, so we use datetime.fromtimestamp directly.
-DatetimeUnix = Annotated[
-    datetime.datetime,
-    BeforeValidator(_datetime_validator),
-    PlainSerializer(lambda x: int(x.timestamp()), return_type=int),
-]
-"""datetime.datetime serialized as int(unixtimestamp in seconds) in db."""
-
-DatetimeUnixNS = Annotated[
+DatetimeUnixTimestamp = Annotated[
     datetime.datetime,
     BeforeValidator(_datetime_validator),
     PlainSerializer(lambda x: x.timestamp(), return_type=float),
 ]
-"""datetime.datetime serialized as float(unixtimestamp in nanoseconds) in db."""
+"""datetime.datetime serialized as unixtimestamp in seconds stored as a float in db."""
+
+DatetimeUnixTimestampInt = Annotated[
+    datetime.datetime,
+    BeforeValidator(_datetime_validator),
+    PlainSerializer(lambda x: int(x.timestamp()), return_type=int),
+]
+"""datetime.datetime serialized as unixtimestamp in seconds stored as an int in db."""
 
 DatetimeISO8601 = Annotated[
     datetime.datetime,
