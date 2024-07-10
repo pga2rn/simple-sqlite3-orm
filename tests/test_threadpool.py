479ff1ac978f300e1ed8ad70c6d86c9cdfed5e1d
pga2rn <aaronpigybd@gmail.com>
2024-07-10 19:14:56 +0900

    test_threadpool: also test select and delete

--- tests/test_threadpool.py
+++ tests/test_threadpool.py
@@ -76,7 +76,7 @@ class TestWithSampleDBAndThreadPool:
 
         logger.info("confirm data written")
         for _selected_entry_count, _entry in enumerate(
-            thread_pool.orm_select_entries(_return_as_generator=True), start=1
+            thread_pool.orm_select_entries_gen(), start=1
         ):
             _corresponding_item = self.data_for_test[_entry.prim_key]
             assert _corresponding_item == _entry
@@ -89,3 +89,43 @@ class TestWithSampleDBAndThreadPool:
             index_keys=INDEX_KEYS,
             unique=True,
         )
+
+    def test_lookup_entries(self, thread_pool: SampleDBConnectionPool):
+        logger.info("test lookup entries")
+        for _entry in self.entries_to_lookup:
+            _looked_up = thread_pool.orm_select_entries(
+                key_id=_entry.key_id,
+                prim_key_sha256hash=_entry.prim_key_sha256hash,
+            )
+            _looked_up = list(_looked_up)
+            assert len(_looked_up) == 1
+            assert _looked_up[0] == _entry
+
+    def test_delete_entries(self, thread_pool: SampleDBConnectionPool):
+        logger.info("test remove and confirm the removed entries")
+        if sqlite3.sqlite_version_info < (3, 35, 0):
+            logger.warning(
+                (
+                    "Current runtime sqlite3 lib version doesn't support RETURNING statement:"
+                    f"{sqlite3.version_info=}, needs 3.35 and above. "
+                    "The test of RETURNING statement will be skipped here."
+                )
+            )
+
+            for entry in self.entries_to_remove:
+                _res = thread_pool.orm_delete_entries(
+                    key_id=entry.key_id,
+                    prim_key_sha256hash=entry.prim_key_sha256hash,
+                )
+                assert _res == 1
+        else:
+            for entry in self.entries_to_remove:
+                _res = thread_pool.orm_delete_entries(
+                    _returning_cols="*",
+                    key_id=entry.key_id,
+                    prim_key_sha256hash=entry.prim_key_sha256hash,
+                )
+                assert isinstance(_res, list)
+
+                assert len(_res) == 1
+                assert _res[0] == entry
