13e2c1628186062e52774b6604bd1de7e2009464
pga2rn <aaronpigybd@gmail.com>
2024-12-16 01:53:21 +0900

    orm_async: finish up re-implement

--- src/simple_sqlite3_orm/_orm/_async.py
+++ src/simple_sqlite3_orm/_orm/_async.py
@@ -1,38 +1,113 @@
 from __future__ import annotations
 
 import asyncio
+import atexit
 import logging
 import sqlite3
-from typing import (
-    Any,
-    AsyncGenerator,
-    Callable,
-    Iterable,
-    Literal,
-    TypeVar,
-)
+from collections.abc import AsyncGenerator, Callable, Generator
+from typing import Any, Generic, TypeVar
+from weakref import WeakValueDictionary
 
-from typing_extensions import ParamSpec, deprecated
+from typing_extensions import Concatenate, ParamSpec
 
-from simple_sqlite3_orm._orm import _multi_thread as _orm_multi_thread
 from simple_sqlite3_orm._orm._base import RowFactorySpecifier
 from simple_sqlite3_orm._orm._multi_thread import ORMBase, ORMThreadPoolBase
-from simple_sqlite3_orm._sqlite_spec import INSERT_OR
-from simple_sqlite3_orm._table_spec import TableSpecType
+from simple_sqlite3_orm._table_spec import TableSpec, TableSpecType
+from simple_sqlite3_orm._utils import GenericAlias
 
 logger = logging.getLogger(__name__)
 
 P = ParamSpec("P")
 RT = TypeVar("RT")
 
+_parameterized_orm_cache: WeakValueDictionary[
+    tuple[type[AsyncORMThreadPoolBase], type[TableSpec]],
+    type[AsyncORMThreadPoolBase[Any]],
+] = WeakValueDictionary()
 
-class AsyncORMThreadPoolBase(ORMThreadPoolBase[TableSpecType]):
+_global_shutdown = False
+
+
+def _python_exit():
+    global _global_shutdown
+    _global_shutdown = True
+
+
+atexit.register(_python_exit)
+
+_SENTINEL = object()
+
+
+def _wrap_with_async_ctx(
+    func: Callable[Concatenate[ORMBase, P], RT],
+):
+    async def _wrapped(
+        self: AsyncORMThreadPoolBase, *args: P.args, **kwargs: P.kwargs
+    ) -> RT:
+        _orm_threadpool = self._orm_threadpool
+
+        def _in_thread() -> RT:
+            _orm_base = _orm_threadpool._thread_scope_orm
+            return func(_orm_base, *args, **kwargs)
+
+        return await asyncio.wrap_future(
+            _orm_threadpool._pool.submit(_in_thread),
+            loop=self._loop,
+        )
+
+    _wrapped.__doc__ = func.__doc__
+    return _wrapped
+
+
+def _wrap_generator_with_async_ctx(
+    func: Callable[Concatenate[ORMBase, P], Generator[TableSpecType]],
+):
+    async def _wrapped(self: AsyncORMThreadPoolBase, *args: P.args, **kwargs: P.kwargs):
+        _orm_threadpool = self._orm_threadpool
+        _async_queue = asyncio.Queue()
+
+        def _in_thread():
+            global _global_shutdown
+            _orm_base = _orm_threadpool._thread_scope_orm
+            try:
+                for entry in func(_orm_base, *args, **kwargs):
+                    if _global_shutdown:
+                        return
+                    self._loop.call_soon_threadsafe(_async_queue.put_nowait, entry)
+            except Exception as e:
+                self._loop.call_soon_threadsafe(_async_queue.put_nowait, e)
+            finally:
+                self._loop.call_soon_threadsafe(_async_queue.put_nowait, _SENTINEL)
+
+        self._orm_threadpool._pool.submit(_in_thread)
+
+        async def _gen() -> AsyncGenerator[TableSpecType]:
+            while not _global_shutdown:
+                entry = await _async_queue.get()
+                if entry is _SENTINEL:
+                    return
+
+                if isinstance(entry, Exception):
+                    try:
+                        raise entry
+                    finally:
+                        entry = None
+                yield entry
+
+        return _gen()
+
+    return _wrapped
+
+
+class AsyncORMThreadPoolBase(Generic[TableSpecType]):
     """
     NOTE: the supoprt for async ORM is experimental! The APIs might be changed a lot
         in the following releases.
 
     For the row_factory arg, please see ORMBase.__init__ for more details.
     """
+
+    orm_table_spec: type[TableSpecType]
 
     def __init__(
         self,
@@ -45,7 +120,7 @@ class AsyncORMThreadPoolBase(ORMThreadPoolBase[TableSpecType]):
         row_factory: RowFactorySpecifier = "table_spec",
     ) -> None:
         # setup the thread pool
-        super().__init__(
+        self._orm_threadpool = ORMThreadPoolBase[self.orm_table_spec](
             table_name,
             schema_name,
             con_factory=con_factory,
@@ -56,189 +131,36 @@ class AsyncORMThreadPoolBase(ORMThreadPoolBase[TableSpecType]):
 
         self._loop = asyncio.get_running_loop()
 
-    def _run_in_pool(
-        self, func: Callable[P, RT], *args: P.args, **kwargs: P.kwargs
-    ) -> asyncio.Future[RT]:
-        """Run normal function in threadpool and track the result async."""
-        return asyncio.wrap_future(
-            self._pool.submit(func, *args, **kwargs),
-            loop=self._loop,
+    def __class_getitem__(cls, params: Any | type[Any] | type[TableSpecType]) -> Any:
+        # just for convienience, passthrough anything that is not type[TableSpecType]
+        #   to Generic's __class_getitem__ and return it.
+        # Typically this is for subscript ORMBase with TypeVar or another Generic.
+        if not (isinstance(params, type) and issubclass(params, TableSpec)):
+            return super().__class_getitem__(params)  # type: ignore
+
+        key = (cls, params)
+        if _cached_type := _parameterized_orm_cache.get(key):
+            return GenericAlias(_cached_type, params)
+
+        new_parameterized_ormbase: type[AsyncORMThreadPoolBase] = type(
+            f"{cls.__name__}[{params.__name__}]", (cls,), {}
         )
+        new_parameterized_ormbase.orm_table_spec = params  # type: ignore
+        _parameterized_orm_cache[key] = new_parameterized_ormbase
+        return GenericAlias(new_parameterized_ormbase, params)
 
-    @property
-    @deprecated("orm_con is not available in thread pool ORM")
-    def orm_con(self):
-        """Not implemented, orm_con is not available in thread pool ORM."""
-        raise NotImplementedError("orm_con is not available in thread pool ORM")
-
-    async def orm_execute(
-        self, sql_stmt: str, params: tuple[Any, ...] | dict[str, Any] | None = None
-    ) -> list[Any]:
-        return await self._run_in_pool(ORMBase.orm_execute, self, sql_stmt, params)
-
-    orm_execute.__doc__ = ORMBase.orm_execute.__doc__
-
-    def orm_select_entries_gen(
-        self,
-        *,
-        _distinct: bool = False,
-        _order_by: tuple[str | tuple[str, Literal["ASC", "DESC"]], ...] | None = None,
-        _limit: int | None = None,
-        **col_values: Any,
-    ) -> AsyncGenerator[TableSpecType, Any]:
-        """Select multiple entries and return an async generator for yielding entries from."""
-        _async_queue = asyncio.Queue()
-
-        def _inner():
-            global _global_shutdown
-            try:
-                for entry in ORMBase.orm_select_entries(
-                    self,
-                    _distinct=_distinct,
-                    _order_by=_order_by,
-                    _limit=_limit,
-                    **col_values,
-                ):
-                    if _orm_multi_thread._global_shutdown:
-                        break
-                    self._loop.call_soon_threadsafe(_async_queue.put_nowait, entry)
-            except Exception as e:
-                self._loop.call_soon_threadsafe(_async_queue.put_nowait, e)
-            finally:
-                self._loop.call_soon_threadsafe(_async_queue.put_nowait, None)
-
-        self._pool.submit(_inner)
-
-        async def _gen():
-            while entry := await _async_queue.get():
-                if isinstance(entry, Exception):
-                    try:
-                        raise entry from None
-                    finally:
-                        del entry
-                yield entry
-
-        return _gen()
-
-    async def orm_select_entries(
-        self,
-        *,
-        _distinct: bool = False,
-        _order_by: tuple[str | tuple[str, Literal["ASC", "DESC"]], ...] | None = None,
-        _limit: int | None = None,
-        **col_values: Any,
-    ) -> list[TableSpecType]:
-        """Select multiple entries and return all the entries in a list."""
-
-        def _inner():
-            return list(
-                ORMBase.orm_select_entries(
-                    self,
-                    _distinct=_distinct,
-                    _order_by=_order_by,
-                    _limit=_limit,
-                    **col_values,
-                )
-            )
-
-        return await self._run_in_pool(_inner)
-
-    async def orm_select_entry(
-        self,
-        *,
-        _distinct: bool = False,
-        _order_by: tuple[str | tuple[str, Literal["ASC", "DESC"]], ...] | None = None,
-        **col_values: Any,
-    ) -> TableSpecType | None:
-        return await self._run_in_pool(
-            ORMBase.orm_select_entry,
-            self,
-            _distinct=_distinct,
-            _order_by=_order_by,
-            **col_values,
-        )
-
-    orm_select_entry.__doc__ = ORMBase.orm_select_entry
-
-    async def orm_delete_entries(
-        self,
-        *,
-        _order_by: tuple[str | tuple[str, Literal["ASC", "DESC"]]] | None = None,
-        _limit: int | None = None,
-        _returning_cols: tuple[str, ...] | None | Literal["*"] = None,
-        **cols_value: Any,
-    ) -> list[TableSpecType] | int:
-        # NOTE(20240708): currently we don't support async generator for delete with RETURNING statement
-        def _inner():
-            res = ORMBase.orm_delete_entries(
-                self,
-                _order_by=_order_by,
-                _limit=_limit,
-                _returning_cols=_returning_cols,
-                **cols_value,
-            )
-
-            if isinstance(res, int):
-                return res
-            return list(res)
-
-        return await self._run_in_pool(_inner)
-
-    orm_delete_entries.__doc__ = ORMBase.orm_delete_entries.__doc__
-
-    async def orm_create_table(
-        self,
-        *,
-        allow_existed: bool = False,
-        strict: bool = False,
-        without_rowid: bool = False,
-    ) -> None:
-        return await self._run_in_pool(
-            ORMBase.orm_create_table,
-            self,
-            allow_existed=allow_existed,
-            strict=strict,
-            without_rowid=without_rowid,
-        )
-
-    orm_create_table.__doc__ = ORMBase.orm_create_table.__doc__
-
-    async def orm_create_index(
-        self,
-        *,
-        index_name: str,
-        index_keys: tuple[str, ...],
-        allow_existed: bool = False,
-        unique: bool = False,
-    ) -> None:
-        return await self._run_in_pool(
-            ORMBase.orm_create_index,
-            self,
-            index_name=index_name,
-            index_keys=index_keys,
-            allow_existed=allow_existed,
-            unique=unique,
-        )
-
-    orm_create_index.__doc__ = ORMBase.orm_create_index.__doc__
-
-    async def orm_insert_entries(
-        self, _in: Iterable[TableSpecType], *, or_option: INSERT_OR | None = None
-    ) -> int:
-        return await self._run_in_pool(
-            ORMBase.orm_insert_entries, self, _in, or_option=or_option
-        )
-
-    orm_insert_entries.__doc__ = ORMBase.orm_insert_entries.__doc__
-
-    async def orm_insert_entry(
-        self, _in: TableSpecType, *, or_option: INSERT_OR | None = None
-    ) -> int:
-        return await self._run_in_pool(
-            ORMBase.orm_insert_entry, self, _in, or_option=or_option
-        )
-
-    orm_insert_entry.__doc__ = ORMBase.orm_insert_entry.__doc__
-
-    def orm_select_all_with_pagination(self, *, batch_size: int):
-        raise NotImplementedError
+    orm_execute = _wrap_with_async_ctx(ORMBase.orm_execute)
+    orm_create_table = _wrap_with_async_ctx(ORMBase.orm_create_table)
+    orm_create_index = _wrap_with_async_ctx(ORMBase.orm_create_index)
+    orm_select_entries = _wrap_generator_with_async_ctx(ORMBase.orm_select_entries)
+    orm_select_entry = _wrap_with_async_ctx(ORMBase.orm_select_entry)
+    orm_insert_entries = _wrap_with_async_ctx(ORMBase.orm_insert_entries)
+    orm_insert_entry = _wrap_with_async_ctx(ORMBase.orm_insert_entry)
+    orm_delete_entries = _wrap_with_async_ctx(ORMBase.orm_delete_entries)
+    orm_delete_entries_with_returning = _wrap_generator_with_async_ctx(
+        ORMBase.orm_delete_entries_with_returning
+    )
+    orm_select_all_with_pagination = _wrap_generator_with_async_ctx(
+        ORMBase.orm_select_all_with_pagination
+    )
+    orm_check_entry_exist = _wrap_with_async_ctx(ORMBase.orm_check_entry_exist)
