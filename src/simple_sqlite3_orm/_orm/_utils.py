31c84c2c5ae9657703e7fea6490820fc3e72e152
pga2rn <aaronpigybd@gmail.com>
2024-12-22 16:16:54 +0900

    orm.utils: fix handling non TableSpec type subscription

--- src/simple_sqlite3_orm/_orm/_utils.py
+++ src/simple_sqlite3_orm/_orm/_utils.py
@@ -12,21 +12,29 @@ _parameterized_orm_cache: WeakValueDictionary[
 
 
 def parameterized_class_getitem(
-    cls, params: Any | type[Any] | type[TableSpecType]
+    cls, params: Any | type[Any] | type[TableSpecType] | tuple[type[Any], ...]
 ) -> Any:
-    # just for convienience, passthrough anything that is not type[TableSpecType]
-    #   to Generic's __class_getitem__ and return it.
-    # Typically this is for subscript ORMBase with TypeVar or another Generic.
-    if not (isinstance(params, type) and issubclass(params, TableSpec)):
-        return super(cls).__class_getitem__(params)
+    if isinstance(params, tuple) or not isinstance(params, type):
+        raise TypeError(
+            f"{cls.__name__} only allows to be parameterized with exactly one type, but get {params=}"
+        )
 
     key = (cls, params)
     if _cached_type := _parameterized_orm_cache.get(key):
         return GenericAlias(_cached_type, params)
 
-    new_parameterized_ormbase: type[Any] = type(
-        f"{cls.__name__}[{params.__name__}]", (cls,), {}
-    )
-    new_parameterized_ormbase.orm_table_spec = params
-    _parameterized_orm_cache[key] = new_parameterized_ormbase
-    return GenericAlias(new_parameterized_ormbase, params)
+    # make a new type from the param
+    if issubclass(params, TableSpec):
+        new_parameterized_ormbase: type[Any] = type(
+            f"{cls.__name__}[{params.__name__}]", (cls,), {}
+        )
+        new_parameterized_ormbase.orm_table_spec = params
+        _parameterized_orm_cache[key] = new_parameterized_ormbase
+        return GenericAlias(new_parameterized_ormbase, params)
+
+    # just for convienience, for anything that is not type[TableSpecType],
+    #   wrap it with GenericAlias.
+    # actually we should only accept TypeVars or Generic, but that is not a very big problem,
+    #   so just leave the current implementation as it.
+    # typically this is for typing purpose.
+    return GenericAlias(cls, params)
