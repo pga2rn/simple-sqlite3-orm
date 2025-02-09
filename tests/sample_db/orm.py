1b6dcb303dbdac2ae262897e807bcb237f707ac3
pga2rn <aaronpigybd@gmail.com>
2025-02-10 00:54:51 +0900

    update tests.sample_db.orm

--- tests/sample_db/orm.py
+++ tests/sample_db/orm.py
@@ -2,9 +2,24 @@
 
 from __future__ import annotations
 
-from simple_sqlite3_orm import ORMBase
+from simple_sqlite3_orm import AsyncORMBase, ORMBase, ORMThreadPoolBase
+from tests.conftest import TABLE_NAME
 from tests.sample_db.table import SampleTable
 
 
 class SampleDB(ORMBase[SampleTable]):
     """ORM for SampleTable."""
+
+    orm_boostrap_table_name = TABLE_NAME
+
+
+class SampleDBAsyncio(AsyncORMBase[SampleTable]):
+    """Test connection pool with async API."""
+
+    orm_boostrap_table_name = TABLE_NAME
+
+
+class SampleDBConnectionPool(ORMThreadPoolBase[SampleTable]):
+    """Test connection pool."""
+
+    orm_boostrap_table_name = TABLE_NAME
