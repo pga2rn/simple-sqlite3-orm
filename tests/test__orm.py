24e3e5615100eed32f502b2925ffa81404952fbc
pga2rn <aaronpigybd@gmail.com>
2025-02-10 01:33:58 +0900

    test__orm: add test for bootstrap API

--- tests/test__orm.py
+++ tests/test__orm.py
@@ -7,17 +7,13 @@ from datetime import datetime
 
 import pytest
 
-from simple_sqlite3_orm import CreateIndexParams, CreateTableParams, ORMBase
+from simple_sqlite3_orm import CreateIndexParams, CreateTableParams
 from tests.conftest import SELECT_ALL_BATCH_SIZE, _generate_random_str
 from tests.sample_db._types import Mystr
 from tests.sample_db.orm import SampleDB
 from tests.sample_db.table import SampleTable
 
 logger = logging.getLogger(__name__)
-
-
-class ORMTest(ORMBase[SampleTable]):
-    _orm_table_name = "test_orm_table"
 
 
 _cur_timestamp = time.time()
@@ -48,17 +44,17 @@ class TestORMBase:
     @pytest.fixture(scope="class")
     def setup_connection(self):
         with sqlite3.connect(":memory:") as conn:
-            orm_inst = ORMTest(conn)
+            orm_inst = SampleDB(conn)
             yield orm_inst
 
     def test_create_without_rowid_table(self):
         """NOTE: to test select_all_with_pagination, we cannot specify without_rowid, so we
         create this test case dedicated for creating without_rowid table test."""
         with sqlite3.connect(":memory:") as conn:
-            orm_inst = ORMTest(conn)
+            orm_inst = SampleDB(conn)
             orm_inst.orm_create_table(without_rowid=True)
 
-    def test_create_table(self, setup_connection: ORMTest):
+    def test_create_table(self, setup_connection: SampleDB):
         setup_connection.orm_create_table(allow_existed=False)
 
         if sqlite3.sqlite_version_info < (3, 37, 0):
@@ -73,7 +69,7 @@ class TestORMBase:
         with pytest.raises(sqlite3.DatabaseError):
             setup_connection.orm_create_table(allow_existed=False)
 
-    def test_create_index(self, setup_connection: ORMTest):
+    def test_create_index(self, setup_connection: SampleDB):
         setup_connection.orm_create_index(
             index_name="idx_prim_key_sha256hash",
             index_keys=("prim_key_sha256hash",),
@@ -88,7 +84,7 @@ class TestORMBase:
                 allow_existed=False,
             )
 
-    def test_insert_entries(self, setup_connection: ORMTest):
+    def test_insert_entries(self, setup_connection: SampleDB):
         setup_connection.orm_insert_entries((entry_for_test,))
 
         with pytest.raises(sqlite3.DatabaseError):
@@ -96,7 +92,7 @@ class TestORMBase:
         setup_connection.orm_insert_entry(entry_for_test, or_option="ignore")
         setup_connection.orm_insert_entry(entry_for_test, or_option="replace")
 
-    def test_orm_execute(self, setup_connection: ORMTest):
+    def test_orm_execute(self, setup_connection: SampleDB):
         sql_stmt = setup_connection.orm_table_spec.table_select_stmt(
             select_from=setup_connection.orm_table_name,
             select_cols="*",
@@ -106,14 +102,14 @@ class TestORMBase:
         res = setup_connection.orm_execute(sql_stmt)
         assert res and res[0][0] > 0
 
-    def test_orm_check_entry_exist(self, setup_connection: ORMTest):
+    def test_orm_check_entry_exist(self, setup_connection: SampleDB):
         assert setup_connection.orm_check_entry_exist(prim_key=entry_for_test.prim_key)
         assert not setup_connection.orm_check_entry_exist(prim_key=Mystr("not_exist"))
 
-    def test_select_entry(self, setup_connection: ORMTest):
+    def test_select_entry(self, setup_connection: SampleDB):
         assert entry_for_test == setup_connection.orm_select_entry(prim_key=mstr)
 
-    def test_select_entries(self, setup_connection: ORMTest):
+    def test_select_entries(self, setup_connection: SampleDB):
         select_result = setup_connection.orm_select_entries(
             _distinct=True,
             _order_by=(("key_id", "DESC"),),
@@ -125,7 +121,7 @@ class TestORMBase:
         assert len(select_result) == 1
         assert select_result[0] == entry_for_test
 
-    def test_select_all_entries(self, setup_connection: ORMTest):
+    def test_select_all_entries(self, setup_connection: SampleDB):
         select_result = setup_connection.orm_select_all_with_pagination(
             batch_size=SELECT_ALL_BATCH_SIZE
         )
@@ -134,13 +130,13 @@ class TestORMBase:
         assert len(select_result) == 1
         assert select_result[0] == entry_for_test
 
-    def test_function_call(self, setup_connection: ORMTest):
+    def test_function_call(self, setup_connection: SampleDB):
         with setup_connection.orm_con as con:
-            cur = con.execute(f"SELECT count(*) FROM {ORMTest._orm_table_name};")
+            cur = con.execute(f"SELECT count(*) FROM {SampleDB._orm_table_name};")
             res = cur.fetchone()
             assert res[0] == 1
 
-    def test_delete_entries(self, setup_connection: ORMTest):
+    def test_delete_entries(self, setup_connection: SampleDB):
         assert setup_connection.orm_delete_entries(key_id=entry_for_test.key_id) == 1
 
 
