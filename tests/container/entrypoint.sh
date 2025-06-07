367fadb209f5b4d5bb570b4da39966dab0d08bfe
pga2rn <aaronpigybd@gmail.com>
2025-06-07 06:13:35 +0000

    minor fix

--- tests/container/entrypoint.sh
+++ tests/container/entrypoint.sh
@@ -7,11 +7,11 @@ OUTPUT_DIR="${OUTPUT_DIR:-/test_result}"
 
 # copy the source code as source is read-only
 cp -R "${SRC}/src" "${TEST_ROOT}"
-cp -R "${SRC}/tests"
-cp "${SRC}/pyproject.toml"
-cp "${SRC}/ruff.toml" || true
-cp "${SRC}/hatch.toml" || true
-cp "${SRC}/uv.lock" || true
+cp -R "${SRC}/tests" "${TEST_ROOT}"
+cp "${SRC}/pyproject.toml" "${TEST_ROOT}"
+cp "${SRC}/ruff.toml" "${TEST_ROOT}" || true
+cp "${SRC}/hatch.toml" "${TEST_ROOT}" || true
+cp "${SRC}/uv.lock" "${TEST_ROOT}" || true
 
 cd ${TEST_ROOT}
 uv run --python python3 --no-managed-python coverage run -m pytest --junit-xml=${OUTPUT_DIR}/pytest.xml ${@:-}
